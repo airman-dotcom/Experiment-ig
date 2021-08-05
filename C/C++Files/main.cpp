64d8c7415a6a1412b356d49df5b230e536a88dfa
SUR-OLP <surtheaagamer17@gmail.com>
2021-08-05 10:52:29 -0400

    added files:batch, and scripts

---
+++ C/C++Files/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+
+
+int main(){
+
+
+
+std::cout<<"This is a c++ file"<<std::endl;
+
+    return 0; 
+}
---
+++ c#Files/main.cs
@@ -0,0 +1,9 @@
+using System;
+					
+public class Program
+{
+	public static void Main()
+	{
+		Console.WriteLine("This is csharp file");
+	}
+}
